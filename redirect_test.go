66af25c071ec28ba3a1fa03d16c9282d209abfe8
Noooste <bloodsword002@gmail.com>
2023-08-20 22:52:48 +0200

    [~] fix redirect test

--- redirect_test.go
+++ redirect_test.go
@@ -5,18 +5,18 @@ import "testing"
 func TestRedirect(t *testing.T) {
 	session := NewSession()
 
-	response, err := session.Get("https://www.nike.com/")
+	response, err := session.Get("https://httpbin.org/status/302")
 
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	if response.StatusCode != 200 {
-		t.Fatal("TestRedirect failed, expected: 200, got: ", response.StatusCode)
+		t.Fatal("TestRedirect2_307 failed, expected: 200, got: ", response.StatusCode)
 	}
 
-	if response.Url == "https://www.nike.com/" {
-		t.Fatal("TestRedirect failed, still on", response.Url)
+	if response.Url != "https://httpbin.org/get" {
+		t.Fatal("TestRedirect2_307 failed, expected: https://httpbin.org/get, got: ", response.Url)
 	}
 }
 
