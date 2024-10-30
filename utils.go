9da5b867e29ed840946ad0f26e08bc16c12ab8bd
Noooste <83548733+Noooste@users.noreply.github.com>
2024-10-30 12:10:26 +0100

    chore(chrome): update to latest version

--- utils.go
+++ utils.go
@@ -24,7 +24,7 @@ const (
 	Socks5      = "socks5"
 	Socks5H     = "socks5h"
 
-	defaultUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
+	defaultUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
 )
 
 var (
