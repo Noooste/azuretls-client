45483605048cada3bfd266a955d335475425af3f
Noooste <83548733+Noooste@users.noreply.github.com>
2024-11-17 23:47:52 +0100

    chore(http2): fix example function

--- example_test.go
+++ example_test.go
@@ -179,7 +179,7 @@ func ExampleSession_ApplyJa3() {
 func ExampleSession_ApplyHTTP2() {
 	session := azuretls.NewSession()
 
-	preset := "1:65536,2:0,3:1000,4:6291456,6:262144|15663105|0|m,s,a,p"
+	preset := "1:65536;2:0;3:1000;4:6291456;6:262144|15663105|0|m,s,a,p"
 
 	if err := session.ApplyHTTP2(preset); err != nil {
 		panic(err)
